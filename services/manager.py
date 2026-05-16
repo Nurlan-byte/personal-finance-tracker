b3f0f9850966e9273c660d5a85b32091b71264f1
Yussupov Nurlan <noooorlan2007@gmail.com>
2026-05-16 23:25:07 +0500

    feat: implement Expense class and enhance Manager for transaction handling

--- services/manager.py
+++ services/manager.py
@@ -1,5 +1,19 @@
-def manager():
-    print("Start")
-    
-if __name__ == "__main__":
-    manager()
+from models.incomes import Income
+from models.expenses import Expense
+
+class Manager:
+    def __init__(self):
+        
+        self.transactions = []
+        
+    def add_transaction(self, transaction):
+        self.transactions.append(transaction)
+        
+    def get_balance(self):
+        sum = 0
+        for i in self.transactions:
+            if isinstance(i, Income):
+                balance += i.get_amount()
+            elif isinstance(i, Expense):
+                balance -= i.get_amount()
+        return balance
