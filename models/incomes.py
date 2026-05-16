454fff386fe29d3d95403ac4f647166c070b62d9
Yussupov Nurlan <noooorlan2007@gmail.com>
2026-05-16 23:15:28 +0500

    refactor: update Income and Transaction classes for improved attribute handling

--- models/incomes.py
+++ models/incomes.py
@@ -3,5 +3,10 @@ from transactions import Transaction
 
 class Income(Transaction):
     
-    def __init__(self):
-        pass
+    def __init__(self, amount, date, inc_category = "general"):
+        super().__init__(amount, date)
+        self.inc_category = inc_category
+        
+    def get_other(self):
+        return super().get_other()
+    
--- models/transactions.py
+++ models/transactions.py
@@ -2,22 +2,22 @@ class Transaction:
     _id_counter = 1
     
     def __init__(self,  amount, date="14.05.2026"):
-        self.__amount = amount
+        self._amount = amount
         self.__id = Transaction._id_counter
-        self.date = date
+        self._date = date
         
         
     def set_date(self, date):
-        self.date = date
+        self._date = date
     
     def get_date(self):
-        return self.date
+        return self._date
     
     def set_amount(self, amount):
-        self.amount = amount
+        self._amount = amount
     
     def get_amount(self):
-        return self.amount
+        return self._amount
     
     def get_other(self):
         pass
