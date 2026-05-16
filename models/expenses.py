6ba78d0ff574231d3d20c541e037857468b88789
Yussupov Nurlan <noooorlan2007@gmail.com>
2026-05-16 23:25:07 +0500

    feat: implement Expense class and enhance Manager for transaction handling

--- models/expenses.py
+++ models/expenses.py
@@ -4,4 +4,8 @@ from transactions import Transaction
 class Expense(Transaction):
     
     def __init__(self, category):
-        pass
+        super().__init__(amount, date)
+        self.category = category
+        
+    def get_other(self):
+        return super().get_other()
