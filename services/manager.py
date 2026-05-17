eab812e9a512ee93b4bb7c5131fff6aae84142f4
Yussupov Nurlan <noooorlan2007@gmail.com>
2026-05-17 18:06:50 +0500

    fix: update transaction handling and balance calculation in Manager class

--- services/manager.py
+++ services/manager.py
@@ -1,12 +1,24 @@
+from services import data_service
+from models.expenses import Expense
+from models.incomes import Income
 
 class Manager:
     def __init__(self):
         self.transactions = []
         
+        data = data_service.load_transactions()
+        for transaction in data:
+            if transaction["type"] == "income":
+                new_income = Income(transaction["amount"], transaction["date"], transaction.get("source", "general"))
+                self.transactions.append(new_income)
+            if transaction["type"] == "expense":
+                new_expense = Expense(transaction["amount"], transaction["date"], transaction.get("category", "general"))
+                self.transactions.append(new_expense)
+                
     def add_transaction(self, transaction):
         self.transactions.append(transaction)
 
-        
-    def get_balance(self):
-        return sum(i.sign_amount for i in self.transactions)
+    @property
+    def balance(self):
+        return sum(i.sign_amount() for i in self.transactions)
     
