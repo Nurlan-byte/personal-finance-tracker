45d674606dfdf6fb2cd52971bff9a834ff0c9217
Yussupov Nurlan <noooorlan2007@gmail.com>
2026-05-25 23:19:29 +0500

    cleanup commit (delete duplicates and useless methods)

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def print_menu():
     print("="*30)
 
 def main():
-    print("Welcome to your personal finance tracker Gnom")
+    print("Welcome to your personal finance tracker")
     myfinances = Manager()
 
     while True:
